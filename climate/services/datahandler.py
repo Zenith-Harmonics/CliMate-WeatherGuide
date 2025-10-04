4dd54f83bf4386090d0a2aa6a47e29f8317be061
mihail-gosman <mihailc.gosman@gmail.com>
2025-10-04 21:40:54 +0300

    feat(datahandler): complete implementation (under test)

--- climate/services/datahandler.py
+++ climate/services/datahandler.py
@@ -1,8 +1,8 @@
-import time
-
 import earthaccess
-import xarray as xr
-import datetime
+import xarray
+import pathlib
+import numpy
+from sklearn.linear_model import LinearRegression
 
 
 class Data:
@@ -12,29 +12,60 @@ class Data:
 
     def search_data(self, date="01-01"):
         results = []
-        for i in range(0, 3):
-            result = earthaccess.search_data(
+        for i in range(0, 20):
+            result = []
+            if i < 10:
+                result = earthaccess.search_data(
                 short_name='M2T1NXSLV',
                 temporal=(f"200{i}-{date}", f"200{i}-{date}"),
                 count=10
-            )
+                )
+            else:
+                result = earthaccess.search_data(
+                    short_name='M2T1NXSLV',
+                    temporal=(f"20{i}-{date}", f"20{i}-{date}"),
+                    count=10
+                )
 
             results.append(result)
-
         return results
 
     def download_data(self, results):
-        datasets_path = []
+        datasets_paths = []
         for result in results:
-            datasets_path.append(earthaccess.download(result, self.datasets_path))
+            path = earthaccess.download(result, self.datasets_path)
+            path_str = str(pathlib.Path(path[0]))
+            datasets_paths.append(path_str)
+        return datasets_paths
+
+    def load_data(self, paths, lat = 45.943, lon = 24.96):
+        datasets = []
+        for path in paths:
+            dataset = xarray.open_dataset(path)
+            t2m = dataset['T2M'] - 273.15
+            t2m_point = t2m.sel(lat=lat, lon=lon, method='nearest')
+            datasets.append(t2m_point.values.tolist())
+        return datasets
+
+    def calculate_prediction(self, dataset):
+        years = numpy.array([2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]).reshape(-1, 1)
+        yearly_mean = numpy.array([numpy.mean(day) for day in dataset])
+
+        model = LinearRegression()
+        model.fit(years, yearly_mean)
+        prediction = model.predict(numpy.array([[2025]]))[0]
+
+        return prediction
 
 
-
-
-
-if __name__ == "__main__":
+def do_the_thing(date="01-01", lat=0.0, lon=0.0):
     data = Data()
 
-    results = data.search_data(date="01-02")
-    print(data.download_data(results))
+    results = data.search_data(date)
+    datasets_paths = data.download_data(results)
+    dataset = data.load_data(datasets_paths, lat, lon)
+    print(data.calculate_prediction(dataset))
+
+
+do_the_thing(date="01-01", lat=45.943, lon=24.96)
 
